f7b977cd395e78b756d5f1113189c266c942cdbb
kariamyasser <38008930+kariamyasser@users.noreply.github.com>
2019-07-16 21:55:23 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
  exports.dbConfig = {
-    user: "demo000",
-    password:"K01141913988k.",
-   server:"den1.mssql3.gear.host",
-   database: "demo000",
+    user: "",
+    password:"",
+   server:"",
+   database: "",
     port: 1433,
     pool: {
         max: 100,
@@ -11,14 +11,6 @@
     }
 
 };  
-
-/* exports.dbConfig = {
-    user: "sa",
-    password:"K01141913988k",
-    server:"localhost\\DESKTOP-8LQFHVF",
-    database: "demo",
-    port: 1433
-};  */
 
 exports.webPort= 9000;
 exports.httpMsgsFormat = "JSON";
